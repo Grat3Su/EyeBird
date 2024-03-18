824abd0845ee5fe8cf3e398dd462b3a02b21094e
장수영 <suyoung998@naver.com>
2024-03-19 00:55:06 +0900

    ssafy webrtc project eyebird

---
+++ BE/src/main/java/com/sixback/eyebird/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.sixback.eyebird.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("url")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
